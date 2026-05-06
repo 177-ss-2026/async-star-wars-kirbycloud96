54e1515d0ea53872b2d1bda437d56282347a5325
William <william.mitchell@student.swic.edu>
2026-05-06 15:21:09 -0500

    feat: add getPlanet fnx and print properties to console

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+async function getPlanet(id) {
+  const resp = await fetch(`https://swapi.info/api/planets/${id}/`);
+  const data = await resp.json();
+  console.info(data);
+}
+
+getPlanet(1);
