347d6aa2986e81c428ea38a85997a0c34f638744
William <william.mitchell@student.swic.edu>
2026-05-06 15:29:16 -0500

    feat: enhance getPlanet function with error handling

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 async function getPlanet(id) {
   const resp = await fetch(`https://swapi.info/api/planets/${id}/`);
+
+  if (!resp.ok) {
+    console.error(`Request failed: ${resp.status}`);
+    return;
+  }
+
   const data = await resp.json();
   console.info(data);
 }
 
-getPlanet(1);
+getPlanet(999);
