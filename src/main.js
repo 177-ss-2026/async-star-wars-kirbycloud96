20e732af63c311ab6a42e9499f357a52083cf664
William <william.mitchell@student.swic.edu>
2026-05-06 16:13:03 -0500

    feat: add try catch error for broken links

--- src/main.js
+++ src/main.js
@@ -9,17 +9,22 @@ select.addEventListener("change", async (event) => {
 
   output.textContent = `Loading ${category}...`;
 
-  const resp = await fetch(`https://swapi.info/api/${category}/`);
+  try {
+    const resp = await fetch(`https://swapi.info/api/${category}/`);
 
-  if (!resp.ok) {
-    output.textContent = `Something went wrong. Status: ${resp.status}`;
-    return;
+    if (!resp.ok) {
+      output.textContent = `Something went wrong. Status: ${resp.status}`;
+      return;
+    }
+
+    const data = await resp.json();
+    const html = data
+      .map((item) => `<li>${item.name ? item.name : item.title}</li>`)
+      .join("");
+
+    output.innerHTML = `<ul>${html}</ul>`;
+  } catch (error) {
+    console.error(error);
+    output.textContent = `An error has occurred: ${error.message}`;
   }
-
-  const data = await resp.json();
-  const html = data
-    .map((item) => `<li>${item.name ? item.name : item.title}</li>`)
-    .join("");
-
-  output.innerHTML = `<ul>${html}</ul>`;
 });
